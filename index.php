ead328da5f36f4112b25c541c095883b98642a9d
Retley <85950306+Retley@users.noreply.github.com>
2023-12-01 20:54:17 -0500

    Actualizacion de index

--- index.php
+++ index.php
@@ -8,16 +8,27 @@ $req = str_replace("/ing-web", "", $_SERVER["REQUEST_URI"]);
 
 if($req == "/"){
     $controller -> index();
-} elseif($req == "/test"){
-    $controller -> test();
-}elseif(preg_match('/\/restaurants\/(\d+)/',$req, $match)){
+}
+elseif(preg_match('/\/restaurants\/(\d+)/',$req, $match)){
     $id = $match[1];
     $controller -> restaurant($id);
-} elseif($req == "/login"){
+} 
+elseif($req == "/login"){
     $controller -> login();
-} elseif($req == "/signup"){
+} 
+elseif($req == "/signup"){
     $controller -> signup();
+} 
+elseif($req == "/filter"){
+    $controller -> filter();
 }
-
+elseif($req == "/signout"){
+    $controller -> signout();
+}
+elseif($req == "/book"){
+    $controller -> book();
+} else{
+    $controller -> not_found();
+}
 
 ?>
